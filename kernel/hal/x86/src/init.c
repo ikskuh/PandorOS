7e82363c96efcae126d8f914eeb9fad303344eb3
Felix Queißner <git@mq32.de>
2016-09-24 18:36:14 +0200

    Adds primitive interrupt handling.

--- kernel/hal/x86/src/init.c
+++ kernel/hal/x86/src/init.c
@@ -1,12 +1,29 @@
 #include <stdint.h>
+#include "console.h"
 
-uint64_t gdt[] = {
+struct idt
+{
+	uint16_t offset0; // 0-15	Offset 0-15	Gibt das Offset des ISR innerhalb des Segments an. Wenn der entsprechende Interrupt auftritt, wird eip auf diesen Wert gesetzt.
+	uint16_t selector; // 16-31	Selector	Gibt den Selector des Codesegments an, in das beim Auftreten des Interrupts gewechselt werden soll. Im Allgemeinen ist dies das Kernel-Codesegment (Ring 0).
+	uint8_t zero;
+	uint8_t flags; // 32-34	000 / IST	Gibt im LM den Index in die IST an, ansonsten 0!
+									// 35-39	Reserviert	Wird ignoriert
+									// 40-42	Typ	Gibt die Art des Interrupts an
+									// 43	D	Gibt an, ob es sich um ein 32bit- (1) oder um ein 16bit-Segment (0) handelt.
+									// Im LM: Für 64-Bit LDT 0, ansonsten 1
+									// 44	0	
+									// 45-46	DPL	Gibt das Descriptor Privilege Level an, das man braucht um diesen Interrupt aufrufen zu dürfen.
+									// 47	P	Gibt an, ob dieser Eintrag benutzt wird.
+	uint16_t offset1; // 48-63	Offset 16-31	
+};
+
+static uint64_t gdt[] = {
 	0x00, // NULL descriptor
 	0x00CF9A000000FFFFULL,
 	0x00CF92000000FFFFULL,
 };
 
-uint64_t idt[256];
+static struct idt idt[256];
 
 static void init_gdt();
 static void init_idt();
@@ -15,7 +32,7 @@ void x86_init()
 {
 	init_gdt();
 	
-	// init_idt();
+	init_idt();
 }
 
 static void init_gdt()
@@ -48,11 +65,33 @@ static inline void lidt(void* base, uint16_t size)
 			uint16_t length;
 			void*    base;
 	} __attribute__((packed)) IDTR = { size, base };
-
 	asm ( "lidt %0" : : "m"(IDTR) );  // let the compiler choose an addressing mode
+}
+
+static struct idt idt_entry(void *ep)
+{
+	uint32_t offset = (uint32_t)ep;
+	struct idt result;
+	result.offset0  = (offset >>  0) & 0xFFFF;
+	result.offset1  = (offset >> 16) & 0xFFFF;
+	result.zero     = 0;
+	result.selector = 0x08;
+	result.flags    = 0x8E;
+	return result;
+}
+
+static void loop()
+{
+	printf("Hello Interrupt!\n");
+	while(1);
 }
 
 static void init_idt()
 {
+	for(int i = 0; i < 256; i++)
+	{
+		idt[i] = idt_entry(&loop);
+	}
 	lidt(idt, sizeof(idt));
+	asm volatile ("sti");
 }
