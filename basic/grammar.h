42b6759db29a4f7b268b840091b34ea918561346
Felix Queißner <git@mq32.de>
2016-10-03 22:11:13 +0200

    Removes basic prototype.

--- basic/grammar.h
+++
@@ -1,7 +0,0 @@
-#define TOK_PLUS                            1
-#define TOK_MINUS                           2
-#define TOK_DIVIDE                          3
-#define TOK_TIMES                           4
-#define TOK_BRO                             5
-#define TOK_BRC                             6
-#define TOK_INTEGER                         7
