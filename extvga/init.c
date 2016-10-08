1fea196631fbdc4b195a9477a1faa0b7a6c466b5
Felix Queißner <git@mq32.de>
2016-10-08 18:38:32 +0200

    Adds basics for vga driver dev.

---
+++ extvga/init.c
@@ -0,0 +1,41 @@
+#include <stdint.h>
+#include "multiboot.h"
+
+//*
+struct multiboot_header __attribute__((section ("multiboot"))) multibootHeader __attribute__ ((aligned (4))) = {
+	MULTIBOOT_HEADER_MAGIC, 
+	0x00,
+	-(MULTIBOOT_HEADER_MAGIC + 0x00),
+	
+	// These are only valid if MULTIBOOT_AOUT_KLUDGE is set. 
+	0, // header_addr;
+	0, // load_addr;
+	0, // load_end_addr;
+	0, // bss_end_addr;
+	0, // entry_addr;
+
+	// These are only valid if MULTIBOOT_VIDEO_MODE is set.
+	0, // mode_type;
+	0, // width;
+	0, // height;
+	0, // depth;
+};
+//*/
+
+void init()
+{
+	const char hw[] = "Hello World!";
+	int i;
+	char* video = (char*) 0xb8000;
+
+	// C-Strings haben ein Nullbyte als Abschluss
+	for (i = 0; hw[i] != '\0'; i++) {
+
+		// Zeichen i in den Videospeicher kopieren
+		video[i * 2] = hw[i];
+
+		// 0x07 = Hellgrau auf Schwarz
+		video[i * 2 + 1] = 0x07;
+	}
+	while(1);
+}
